ff5163470bdef08b7df4cdd65bbf418a5fc24c84
qasimfida <qasimfida0054@gmail.com>
2024-05-31 16:40:25 +0500

    integrated clerkjs auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,70 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
-
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import Search from './screens/search.jsx';
-import { ModalProvider } from './components/modal';
-import { useModal } from './contexts/Modal';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Search from "./screens/search.jsx";
+import { ModalProvider } from "./components/modal";
+import { useModal } from "./contexts/Modal";
+import { ClerkProvider, useUser, RedirectToSignIn, SignInButton, SignOutButton } from "@clerk/clerk-react";
+import { CustomButton } from "./components/button";
 
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const Button = () => {
-  const { openModal} = useModal()
-  return (
-    <button onClick={() => openModal()}>hello</button>
-  )
+  const { openModal } = useModal();
+
+  return <button onClick={() => openModal()}>hello</button>;
+};
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
 }
-function initSearchLib(rootElement = document.getElementById('searchLib')) {
+
+const AuthenticatedApp = () => {
+  const { isSignedIn } = useUser();
+
+  if (!isSignedIn) {
+    return (<>
+    <SignInButton/>
+
+    </>
+    
+  );
+  }
+
+  return (
+    <div className="cc-flex cc-flex-col">
+    <SignOutButton/>
+      <Button />
+      <Search />
+    </div>
+  );
+};
+
+function initSearchLib(rootElement = document.getElementById("searchLib")) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
           <ModalProvider>
-            <Button/>
-            <Search />
+            <Router>
+              <Routes>
+                <Route path="/" element={<AuthenticatedApp />} />
+                {/* Add other routes here */}
+              </Routes>
+            </Router>
           </ModalProvider>
-      </QueryClientProvider>
-    </React.StrictMode>,
-  )
+        </QueryClientProvider>
+      </ClerkProvider>
+    </React.StrictMode>
+  );
 }
 
 // if in development mode, render immediately
-  initSearchLib()
+initSearchLib();
 
 window.initSearchLib = initSearchLib;
-
